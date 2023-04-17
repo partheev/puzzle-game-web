6028304f19a2fe612ec3e9db8d62b21ab7af53d7
partheev <partheev8@gmail.com>
2023-04-18 00:46:21 +0530

    project setup

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+
+export const connectDB = async()=>{
+  // establish connection to db
+}
---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use('/api/auth')
+app.listen(process.env.PORT,()=>{
+  console.log('Server running on port '+process.env.PORT)
+})
